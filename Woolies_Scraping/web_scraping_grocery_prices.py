348a002fa34eac2cc14043b3b80c57a08c00d214
Yi Xin Tan <75169586+YiXin-Tan@users.noreply.github.com>
2024-03-03 14:17:34 +1100

    refactored web scrapper file name, reduced wait time and imported get_structured_message into here

--- Woolies_Scraping/web_scraping_grocery_prices.py
+++ Woolies_Scraping/web_scraping_grocery_prices.py
@@ -13,7 +13,7 @@ def fetch_first_product_details(search_term: str) -> dict[int, str, str]:
     product_info = {}
     try:
         # Wait for the 'wc-product-tile' elements to be loaded on the page.
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 3).until(
             EC.presence_of_element_located((By.TAG_NAME, "wc-product-tile"))
         )
 
@@ -46,6 +46,25 @@ def fetch_first_product_details(search_term: str) -> dict[int, str, str]:
         driver.quit()
 
     return product_info
+
+
+def get_structured_message(response_type):
+    json_schema = {
+        "suggested_shopping_list": {
+            "text":{
+                "response_text": "string (recipe suggestions)",
+                "shopping_list": "array of strings (3-10 grocery items). A sample response would be: '{shopping_list: [tomato,minced meat,potato]'}",
+            }
+        },
+
+        "suggested_recipes": {
+            "recipes": "array of strings (2-5 recipes). For example: ['recipe1', 'recipe2', ...]"
+        }
+    }
+
+    return f"""Please respond with your {response_type} directly in a valid JSON format 
+    (without using Markdown code blocks or any other formatting).
+    The JSON schema should include: {json_schema[response_type]}"""
 
 
 if __name__ == '__main__':
